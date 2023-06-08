b57e608a92724e00ced2e83ea641f263f4f738bd
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-06-09 02:45:54 +0530

    Time: 12 ms (39.36%) | Memory: 10 MB (56.86%) - LeetSync

--- check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.cpp
@@ -7,9 +7,10 @@ public:
 
     float slope, c;
     if ((coordinates[1][0] - coordinates[0][0]) != 0) {
-        slope = static_cast<float>(coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
+        slope = float(coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
         c = coordinates[0][1] - (slope * coordinates[0][0]);
-    } else {
+    } 
+    else {
         int x = coordinates[0][0];
         for (int i = 2; i < m; i++) {
             if (coordinates[i][0] != x)
@@ -18,15 +19,11 @@ public:
         return true; // All x-coordinates are the same, so it's a vertical line
     }
 
-    for (int i = 2; i < m; i++) {
-        // if ((coordinates[i][0] - coordinates[i - 1][0]) == 0) {
-        //     if (coordinates[i][0] != coordinates[i - 1][0])
-        //         return false;
-        // } else {
+    for (int i = 2; i < m; i++) 
+    {
             int y = slope * coordinates[i][0] + c;
             if (y != coordinates[i][1])
                 return false;
-        // }
     }
 
     return true;
