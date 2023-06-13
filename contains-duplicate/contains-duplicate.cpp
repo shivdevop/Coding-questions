879538dbbb51f7b36182c416f4b7bce13bf149f4
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-06-14 02:57:39 +0530

    Time: 139 ms (76.72%) | Memory: 57.1 MB (99.63%) - LeetSync

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) 
-    {
-       unordered_map<int,int> mpp;
+    // bool containsDuplicate(vector<int>& nums) 
+    // {
+    //    unordered_map<int,int> mpp;
 
-        for(int i=0;i<nums.size();i++)
-        mpp[nums[i]]++;
+    //     for(int i=0;i<nums.size();i++)
+    //     mpp[nums[i]]++;
 
-        for(int i=0;i<nums.size();i++)
-        {
-            if(mpp[nums[i]]>=2)
-            return true;
-        }
+    //     for(int i=0;i<nums.size();i++)
+    //     {
+    //         if(mpp[nums[i]]>=2)
+    //         return true;
+    //     }
         
+    //     return false;
+    // }
+      bool containsDuplicate(vector<int>& A)
+    {
+        sort(A.begin(),A.end());
+
+        for(int i=1; i<A.size(); i++)
+        {
+            if(A[i]==A[i-1])
+                return true;
+        }
         return false;
     }
 };
