3f850289eabaac34fb49184a4893f76bfa3aa38c
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-07-04 03:29:39 +0530

    Time: 6 ms (23.29%) | Memory: 6.3 MB (43.33%) - LeetSync

---
+++ happy-number/happy-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isHappy(int n) 
+    {   
+        if(n==1)
+        return true;
+        unordered_map<int,int> mpp;
+        mpp[n]++;
+        while(n>1)
+        {
+            int newval=0;
+            while(n>0)
+            {
+              int digit=n%10;
+              n=n/10;
+              newval+=(digit*digit);
+            }
+            n=newval;
+            if(newval==1)
+            return true;
+            else if(mpp.find(newval)!=mpp.end())
+            return false;
+            mpp[newval]++;
+        }
+      return false;
+    }
+};
