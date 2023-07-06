219e2d863f26f371e5da5b18b2f85af2e7dfbba8
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-07-06 16:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 39.6 MB (23.61%) - LeetSync

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n)
+    {   if(n<=2)
+        return n;
+        int prev2=1;
+        int prev=2;
+        int curr;
+        for(int i=3;i<=n;i++)
+        {
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+
+        }
+        return prev;
+        
+    }
+}
