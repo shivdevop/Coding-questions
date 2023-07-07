a6e57791ed5674d3045cb0eeffad5e65de172995
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-07-08 01:39:10 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (49.50%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
-  int f(int row,int col,vector<vector<int>> &dp)
-    {   
+//   int f(int row,int col,vector<vector<int>> &dp)
+//     {   
 
-         if(row<0 || col<0)
-        return 0;
+//          if(row<0 || col<0)
+//         return 0;
 
-        if(dp[row][col]!=-1)
-        return dp[row][col];
+//         if(dp[row][col]!=-1)
+//         return dp[row][col];
 
-        if(row==0 && col==0)
-        return 1;
+//         if(row==0 && col==0)
+//         return 1;
 
        
-        int up=f(row-1,col,dp);
-        int down=f(row,col-1,dp);
-        return dp[row][col]=up + down;
-    }
+//         int up=f(row-1,col,dp);
+//         int left=f(row,col-1,dp);
+//         return dp[row][col]=up + left;
+//     }
+
+//  int uniquePaths(int m, int n)
+//     {
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         int ans=f(m-1,n-1,dp);
+//         return ans;
+        
+//     }
+
+
+//TABULAR METHOD
 
  int uniquePaths(int m, int n)
     {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        int ans=f(m-1,n-1,dp);
-        return ans;
+        
+        //DECLARE BASE CASES 
+        dp[0][0]=1;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)
+                dp[i][j]=1;
+                else
+                {
+                 int bottom=0,right=0;
+                if(i>0) 
+                 bottom=dp[i-1][j];
+                if(j>0)
+                 right=dp[i][j-1];
+                dp[i][j]=bottom+right;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
         
     }
 };
