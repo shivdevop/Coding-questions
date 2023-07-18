9481475a63f2353ffd5afd74fab45204fde94d5e
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-07-19 00:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (36.13%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool checkValidString(string s) 
+    {
+        stack<int> open;
+        stack<int> star;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            open.push(i);
+            else if(s[i]=='*')
+            star.push(i);
+            else
+            {
+                if(!open.empty())
+                open.pop();
+                else if(!star.empty())
+                star.pop();
+                else
+                return false;
+            }
+        }
+
+        if(open.empty())
+        return true;
+        else if(!open.empty() && star.empty())
+        return false;
+
+        while(!open.empty())
+        {   
+            if(star.empty())
+            return false;
+
+            if(star.top()<open.top())
+            return false;
+
+            star.pop();
+            open.pop();
+        }
+        return true;
+    }
+};
