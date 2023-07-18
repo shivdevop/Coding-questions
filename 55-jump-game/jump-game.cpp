c42ec487361c8ad49e91ef04e05c66d817830f5b
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-07-19 03:03:01 +0530

    Time: 1865 ms (5.01%) | Memory: 51.5 MB (5.85%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-    // bool f(int index,vector<int>& nums,vector<int> &dp)
-    // {
-    //     if(index>=nums.size()-1)
-    //     return true;
+    bool f(int index,vector<int>& nums,vector<int> &dp)
+    {
+        if(index>=nums.size()-1)
+        return true;
 
-    //     if(nums[index]==0)
-    //     return false;
+        if(nums[index]==0)
+        return false;
 
-    //     if(dp[index]!=-1)
-    //     return dp[index];
+        if(dp[index]!=-1)
+        return dp[index];
     
-    //     dp[index]=false;
+        dp[index]=false;
 
-    //     for(int i=1;i<=nums[index];i++)
-    //     dp[index]=dp[index] || f(index+i,nums,dp);
+        // for(int i=1;i<=nums[index];i++)
+        // dp[index]=dp[index] || f(index+i,nums,dp); -->this is unnecessary because if one function call can give us true why to unnecessary traverse further !!!!!
 
-    //     return dp[index];
-    // }
-    // bool canJump(vector<int>& nums) {
+           for(int i=1;i<=nums[index];i++)
+           { if(f(index+i,nums,dp))
+                return dp[index]=true;
+           }
 
-    //    int n=nums.size();
-    //    vector<int> dp(n,-1);
-
-    //     bool ans=f(0,nums,dp);//note we cannot do it from the back; the order matters  a lot and can give a completely different answer !!!!!
-    //     return ans;
-        
-    // }
-    bool dfs(int i, int n, vector<int> &nums, vector<int> &dp) {
-        if(i >= n - 1) return true;
-        if(dp[i] != -1) return dp[i];
-
-        for(int j=1; j<=nums[i]; j++) {
-            if(dfs(i + j, n, nums, dp)) return dp[i] = true;
-        }
-        return dp[i] = false;
+        return dp[index];
     }
-     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return dfs(0, n, nums, dp);
+    bool canJump(vector<int>& nums) {
+
+       int n=nums.size();
+       vector<int> dp(n,-1);
+
+        bool ans=f(0,nums,dp);//note we cannot do it from the back; the order matters  a lot and can give a completely different answer !!!!!
+        return ans;
+        
     }
 };
