20e0ef3cbd4d78c381d9de993da2f13d505e04d3
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-07-28 17:28:59 +0530

    Time: 151 ms (99.60%) | Memory: 119 MB (7.21%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+   bool f(int mid,vector<int> bloomDay,int m,int k)
+   {  //mid refers to a certain blooming day 
+      int consecutive=0;
+      int bouquets=0;
+      for(int i=0;i<bloomDay.size();i++)
+      {
+          if(bloomDay[i]<=mid)
+          {
+              consecutive++;//consecutive bloomed flowers count 
+          }
+          else
+          {
+              bouquets+=consecutive/k;
+              consecutive=0;
+          }
+      
+      }
+     bouquets+=consecutive/k;
+     return bouquets>=m;    
+  
+   }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        
+     long long n=bloomDay.size();
+        long long x=(long long)m*k;
+        if(x>n)
+        return -1;
+        int low=INT_MAX;
+        int high=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            low=min(low,bloomDay[i]);
+            high=max(high,bloomDay[i]);
+        }
+         int ans=-1;
+        while(low<=high)
+        { 
+            int mid=(low+high)/2;
+            if(f(mid,bloomDay,m,k))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            low=mid+1;
+          
+        }
+        return ans;
+    }
+};
