263621f9eecec13ccb1833d5ef98a886a259916f
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-08-02 04:25:43 +0530

    Time: 3 ms (35.28%) | Memory: 6 MB (25.57%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maximum69Number (int num)
     {
-         string s=to_string(num);
+         string s=to_string(num);//integer to string directly conversion
          for(int i=0;i<s.length();i++)
          {
              if(s[i]=='6')
             { s[i]='9';
             break;}
          }       
-         num=stoi(s);
+         num=stoi(s);//direct conversion from string to number!!!!
          return num;
 
 
