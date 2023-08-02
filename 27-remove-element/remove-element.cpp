8df03ec1d244097d9a1a2774237e1e08dd6f4542
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-08-03 03:20:53 +0530

    Time: 5 ms (19.33%) | Memory: 8.9 MB (14.00%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int i=0,j=nums.size()-1;
+        while(i<=j)
+        {
+            while(i<=j && nums[i]!=val)
+            i++;
+            while(j>=i && nums[j]==val)
+            j--;
+            if(i<=j)
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+
+        }
+        
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==val)
+            break;
+            count++;
+        }
+        return count;
+    }
+};
