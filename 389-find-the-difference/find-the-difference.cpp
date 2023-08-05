1628f971a8e1af098eefd299530e7b760a872e93
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-08-06 03:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (53.25%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,25 +1,15 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-    // sort(t.begin(),t.end());
-    // int i;
-    // for( i=0;i<s.length();i++)
-    // {
-    //     if(s[i]!=t[i])
-    //     return t[i];
-    // }
-    // return t[i];
-    // }
-      char result = 0;
-    
-    for (char ch : s) {
-        result ^= ch;
+     sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    int i;
+    for( i=0;i<s.length();i++)
+    {
+        if(s[i]!=t[i])
+        return t[i];
     }
-    
-    for (char ch : t) {
-        result ^= ch;
+    return t[i];
     }
-    
-    return result;
-    }
+
 };
