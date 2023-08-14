5ab20d64981a9048fe1755de3fc3108a9839ece5
shivdevop <72686438+shivdevop@users.noreply.github.com>
2023-08-15 00:01:49 +0530

    Time: 2 ms (48.45%) | Memory: 6 MB (10.53%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,25 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+         int low=1,high=n;
+        int ans;
+        while(low<=high)
+        {
+            int mid=((long long)low+high)/2;
+            if(isBadVersion(mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+
+        }
+        return ans;
+    }
+};
